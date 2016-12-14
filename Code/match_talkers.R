40d3b943dba2db79faf8d7052ba981da38cbfed9
tylerascott <tyler.scott@uga.edu>
2016-12-14 13:43:12 -0800

    remote sync

--- Code/match_talkers.R
+++ Code/match_talkers.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 library(stringr)
+library(statnet)
+library(btergm)
+rm(list=ls())
 talkers = read_csv('Input/pdfs/extraction/named_entities.csv') %>% 
   dplyr::select(-X1) %>%
   filter(!grepl('[A-Z]{4}',Subject),!grepl('^[a-z]',Subject)) %>%
@@ -7,30 +10,79 @@ talkers = read_csv('Input/pdfs/extraction/named_entities.csv') %>%
   mutate(Meeting = gsub('\\.txt$','',Meeting)) %>%
   filter(!grepl('Upper Baker',Subject)) %>% mutate(Year = str_extract(Meeting,'[0-9]{4}'))
 
+
 attendance = read_csv('Input/scraped_data/temp_cleaned_data.csv') %>% 
   dplyr::select(-X1) %>% mutate(Year = str_extract(Meeting,'[0-9]{4}'))
+library(lubridate)
+
+
+temp_dates = ymd(str_extract(attendance$Meeting,'[0-9]{8}$'))
+temp_dates[is.na(temp_dates)] = 
+         ymd(str_extract(attendance$Meeting[is.na(temp_dates)],'[0-9]{4}.[0-9]{1,2}.[0-9]{1,2}'))
+
+attendance$Date = temp_dates
+attendance$Dec_Date = decimal_date(attendance$Date)
+
+talkers$Date = attendance$Date[match(talkers$Meeting,attendance$Meeting)]
+talkers$Dec_Date = attendance$Dec_Date[match(talkers$Meeting,attendance$Meeting)]
+
+grep('^[A-Z]',talkers$Verb,value=T)
+
+
+
+phase_break_date = c(mdy('5/8/2003'),mdy('11/24/2004'),mdy('10/17/2008'),mdy('01/01/2015'))
+phase_name = c('planning/scoping','application/settlement development',
+                                          'agency review','license implementation')
+phase_break_ddate  = decimal_date(phase_break_date)
+
+period_break_ddates = seq(decimal_date(mdy('05/01/2000')),decimal_date(mdy('11/01/2014')),0.5)
+
+meeting_master = attendance %>% filter(!duplicated(Meeting)) %>% 
+  dplyr::select(-Name,-Org,-Relevance,-Topic,-Count)
+
+meeting_master$Interval = findInterval(meeting_master$Dec_Date,period_break_ddates)
+meeting_master$Phase = ifelse(meeting_master$Dec_Date < phase_break_ddate[1],1,
+                              ifelse((meeting_master$Dec_Date >= phase_break_ddate[1] &
+                                      meeting_master$Dec_Date < phase_break_ddate[2]), 2,
+                              ifelse((meeting_master$Dec_Date >= phase_break_ddate[2] &
+                                       meeting_master$Dec_Date < phase_break_ddate[3]),3,4)))
+
+# library(gridExtra)
+# g1 = ggplot(meeting_master,aes(x=Dec_Date)) + geom_histogram(breaks=period_breaks$pbreaks) + 
+#   scale_x_continuous(name = '6 month intervals (May to November')+ scale_y_continuous(name = '# observed meetings')
+# g2 = ggplot(meeting_master,aes(x=as.factor(phase))) + geom_bar() + scale_y_continuous(name = '# observed meetings') +
+#   scale_x_discrete(name = 'Phase',labels=as.character(phase_breaks$phase_name))
+# grid.arrange(g1,g2)
+
+period_break_ddates
+attendance = left_join(attendance,meeting_master)
+talkers = left_join(talkers,meeting_master)
 
 match_as_present = lapply(1:length(talkers$Subject),
                           function(x) ifelse(length( grep(talkers$Subject[x],attendance$Name[attendance$Meeting==talkers$Meeting[x]]))==0,NA,
                                              grep(talkers$Subject[x],attendance$Name[attendance$Meeting==talkers$Meeting[x]],value=T)))
 
 talkers = talkers %>% mutate(Subject_Match = unlist(match_as_present)) %>%
-  filter(!is.na(Subject_Match)) %>% group_by(Subject_Match,Meeting,Year) %>% summarise(part_count = n())
+  filter(!is.na(Subject_Match)) %>% group_by(Subject_Match,Meeting,Year,Date,Dec_Date,Interval,Phase) %>% summarise(part_count = n())
+
 
 participation_edges = do.call(rbind,lapply(1:nrow(talkers),function(i)
   data.frame(Participant = talkers$Subject_Match[i],Attendee= attendance$Name[attendance$Meeting==talkers$Meeting[i]],
-             part_count = talkers$part_count[i],Meeting = talkers$Meeting[i],Year = talkers$Year[i])))
+             part_count = talkers$part_count[i],Meeting = talkers$Meeting[i],Year = talkers$Year[i],Date = talkers$Date[i],
+             Dec_Date = talkers$Dec_Date[i],Phase = talkers$Phase[i],Interval = talkers$Interval[i])))
 
-year_engagement_edges = participation_edges %>% group_by(Participant, Attendee, Year) %>% summarise(direct_engagement = sum(part_count))
 
+
+interval_engagement_edges = participation_edges %>% group_by(Participant, Attendee,Interval) %>% 
+  summarise(direct_engagement = sum(part_count))
 
 meeting_attendees = sort(unique(attendance$Name))
 total_people = length(meeting_attendees)
 
-node_base = attendance %>% dplyr::select(-Meeting,-Year) %>%
+node_base = attendance %>% dplyr::select(-Meeting,-Interval) %>%
   filter(!duplicated(.))
 
-node_base$Agency = ifelse(grepl('USFS|FW|DOE|FERC|UCACE|DOT',node_base$Org),1,0)
+node_base$Agency = ifelse(grepl('USFS|FW|DOE|FERC|UCACE|DOT|NMFS|USDA',node_base$Org),1,0)
 node_base$Consultant = ifelse(grepl('Group|Associates|Consulting|QEA|Engin',
                                    node_base$Org),1,0)
 node_base$Utility = ifelse(grepl('PSE',node_base$Org),1,0)
@@ -38,48 +90,56 @@ node_base$Utility = ifelse(grepl('PSE',node_base$Org),1,0)
 base_matrix = matrix(0,ncol=total_people,nrow=total_people)
 colnames(base_matrix) = rownames(base_matrix) = meeting_attendees
 
-net_array = replicate(length(unique(year_engagement_edges$Year)),base_matrix)
-dimnames(net_array)[[3]] = sort(unique(as.character(year_engagement_edges$Year)))
-placement = cbind(match(year_engagement_edges$Participant,meeting_attendees),
-                  match(year_engagement_edges$Attendee,meeting_attendees),
-                  match(year_engagement_edges$Year,dimnames(net_array)[[3]]))
-net_array[placement] = year_engagement_edges$direct_engagement
+net_array = replicate(length(unique(interval_engagement_edges$Interval)),base_matrix)
+dimnames(net_array)[[3]] = sort(unique(as.character(interval_engagement_edges$Interval)))
+placement = cbind(match(interval_engagement_edges$Participant,meeting_attendees),
+                  match(interval_engagement_edges$Attendee,meeting_attendees),
+                  match(interval_engagement_edges$Interval,dimnames(net_array)[[3]]))
+net_array[placement] = interval_engagement_edges$direct_engagement
 
 net_list = lapply(1:dim(net_array)[3],function(x) 
   as.network(net_array[,,x],directed=T,matrix.type='adjacency'))
 
 
-yearly_attendance = lapply(1:ncol(table(attendance$Name,attendance$Year)),function(x)
-  as.data.frame.matrix(table(attendance$Name,attendance$Year)[,replicate(nrow(table(attendance$Name,attendance$Year)),x)],
-            dimnames=list(rownames(table(attendance$Name,attendance$Year)),
-                          rownames(table(attendance$Name,attendance$Year)))))
-for (i in 1:length(yearly_attendance))
-colnames(yearly_attendance[[i]]) =  rownames(table(attendance$Name,attendance$Year))
-yearly_attendance_prior = yearly_attendance[-length(yearly_attendance)]
-yearly_attendance_prior= lapply(yearly_attendance_prior,as.matrix)
-yearly_attendance_prior = lapply(yearly_attendance_prior,sqrt)
+interval_attendance = lapply(1:ncol(table(attendance$Name,attendance$Interval)),function(x)
+  as.data.frame.matrix(table(attendance$Name,attendance$Interval)[,replicate(nrow(table(attendance$Name,attendance$Interval)),x)],
+            dimnames=list(rownames(table(attendance$Name,attendance$Interval)),
+                          rownames(table(attendance$Name,attendance$Interval)))))
+for (i in 1:length(interval_attendance))
+{colnames(interval_attendance[[i]]) =  rownames(table(attendance$Name,attendance$Interval))}
 
+interval_attendance_prior = interval_attendance[-length(interval_attendance)]
+interval_attendance_prior= lapply(interval_attendance_prior,as.matrix)
+interval_attendance_prior = lapply(interval_attendance_prior,sqrt)
 
-yearly_outdegree = apply(net_array,3,rowSums)
+interval_outdegree = apply(net_array,3,rowSums)
 #lapply(1:ncol(yearly_outdegree),function(x) as.data.frame.matrix()
-yearly_outdegree = lapply(1:ncol(yearly_outdegree),function(i)
-cbind(replicate(nrow(yearly_outdegree),yearly_outdegree[,i])))
-for (i in 1:length(yearly_outdegree)){colnames(yearly_outdegree[[i]]) = 
-  rownames(yearly_outdegree[[i]])}
-yearly_outdegree_prior = yearly_outdegree[-length(yearly_outdegree)]
-yearly_outdegree_prior = lapply(yearly_outdegree_prior,sqrt)
+interval_outdegree = lapply(1:ncol(interval_outdegree),function(i)
+cbind(replicate(nrow(interval_outdegree),interval_outdegree[,i])))
+for (i in 1:length(interval_outdegree)){colnames(interval_outdegree[[i]]) = 
+  rownames(interval_outdegree[[i]])}
+interval_outdegree_prior = interval_outdegree[-length(interval_outdegree)]
+interval_outdegree_prior = lapply(interval_outdegree_prior,sqrt)
 
-yearly_outdegree_x_attendance_prior = lapply(1:length(yearly_outdegree_prior),function(x) yearly_outdegree_prior[[x]]*
-         yearly_attendance_prior[[x]])
+interval_outdegree_x_attendance_prior = lapply(1:length(interval_outdegree_prior),function(x) interval_outdegree_prior[[x]]*
+         interval_attendance_prior[[x]])
 
 
-stability_list = lapply(1:dim(net_array)[3],function(x) base_matrix)
-stability_list = lapply(1:16,function(x) base_matrix)
+
+dynamic_list = lapply(1:dim(net_array)[3],function(x) base_matrix)
 for (mat in 2:length(net_list))
 {
-  stability_list[[mat]] <- ifelse(as.sociomatrix(net_list[[mat-1]])>0,1,-1)
+  dynamic_list[[mat]] <- ifelse(as.sociomatrix(net_list[[mat-1]])>0,1,-1)
 }
-stability_list_prior = stability_list[-16]
+dynamic_list_prior = dynamic_list[-dim(net_array)[3]]
+
+stability_list = lapply(1:dim(net_array)[3],function(x) base_matrix)
+for (mat in 2:length(net_list))
+{
+  stability_list[[mat]] <- ifelse(as.sociomatrix(net_list[[mat-1]])>0,1,0)
+}
+stability_list_prior = stability_list[-dim(net_array)[3]]
+
 
 for (x in 1:length(net_list))
 {
@@ -105,12 +165,177 @@ for (x in 2:length(net_list))
   network::get.vertex.attribute(net_list[[x]],'Core_Order_Prior'),
   attrname = 'High_Resource_x_Core_Order_Prior')}
 
-test = btergm(net_list[-1] ~ edges  + mutual + nodecov('High_Resource')+
+
+
+for (x in 2:length(net_list))
+{
+  network::set.vertex.attribute(net_list[[x]],attrname = 'Utility',
+                                value = ifelse(network.vertex.names(net_list[[x]]) %in% node_base$Name[node_base$Utility==1],1,0))
+}
+
+
+node_base$Mandatory = ifelse(grepl("NMFS|USFS|WDOE",node_base$Org),1,0)
+
+for (x in 2:length(net_list))
+{
+  network::set.vertex.attribute(net_list[[x]],attrname = 'Mandatory',
+                                value = ifelse(network.vertex.names(net_list[[x]]) %in% node_base$Name[node_base$Mandatory==1],1,0))
+}
+
+
+utility_matrix_odegree = do.call(cbind,lapply(1:network.size(net_list[[2]]),function(x) get.vertex.attribute(net_list[[2]],'Utility')))
+utility_list_odegree  = lapply(1:length(net_list),function(x) utility_matrix_odegree)
+
+utility_matrix_idegree = do.call(rbind,lapply(1:network.size(net_list[[2]]),function(x) get.vertex.attribute(net_list[[2]],'Utility')))
+utility_list_idegree  = lapply(1:length(net_list),function(x) utility_matrix_idegree)
+
+utility_list_odegree = utility_list_odegree[-1] 
+utility_list_idegree = utility_list_idegree[-1]
+
+
+# signatory_matrix_odegree = do.call(cbind,lapply(1:network.size(net_list[[2]]),function(x) get.vertex.attribute(net_list[[2]],'Signatory')))
+# signatory_list_odegree  = lapply(1:length(net_list),function(x) signatory_matrix_odegree)
+# 
+# signatory_matrix_idegree = do.call(rbind,lapply(1:network.size(net_list[[2]]),function(x) get.vertex.attribute(net_list[[2]],'Signatory')))
+# signatory_list_idegree  = lapply(1:length(net_list),function(x) signatory_matrix_idegree)
+# 
+# signatory_list_odegree = signatory_list_odegree[-1] 
+# signatory_list_idegree = signatory_list_idegree[-1]
+
+
+gwd_decay = 2
+gwesp_decay = 2
+Rnum = 1000
+
+base = "net_list[-1] ~ edges  + mutual + gwidegree(gwd_decay,fixed=T) +  gwodegree(gwd_decay,fixed=T) + gwesp(gwesp_decay,fixed=T)"
+
+h1_block = #H1
+  "edgecov(dynamic_list_prior)+ 
+timecov(x = dynamic_list_prior,  minimum = 8, transform = function(t) 1)"
+
+h2_block =  #H2
+  "nodecov('High_Resource')+nodecov('Core_Order_Prior')+nodecov('High_Resource_x_Core_Order_Prior')"
+
+h3_block =   #H3
+  "edgecov(stability_list_prior) + 
+timecov(x = stability_list_prior,  minimum = 18, transform = function(t) 1)"
+
+h4_block = #h5
+  "timecov(x = utility_list_odegree[-1],function(t) t)"
+
+h5_block =  #H5
+  "nodeofactor('Utility') + nodeifactor('Utility') + nodeofactor('Mandatory') + nodeifactor('Mandatory')" 
+
+
+full_mod = btergm(as.formula(paste(base,h2_block,h5_block,h4_block,h1_block,h5_block,sep='+')),R= Rnum)
+noh5_mod = btergm(as.formula(paste(base,h2_block,h5_block,h4_block,h1_block,sep='+')),R= Rnum)
+noh1_mod = btergm(as.formula(paste(base,h2_block,h5_block,h4_block,h5_block,sep='+')),R= Rnum)
+
+
+
+set.seed(24)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+testA = btergm(as.formula(paste(base,h1_block,sep='+')),R=100)
+testB = btergm(as.formula(paste(base,h3_block,sep='+')),R=100)
+testC = btergm(as.formula(paste(base,h1_block,h3_block,sep='+')),R=100)
+
+summary(testA)
+summary(testB)
+summary(testC)
+
+as.formula(paste(base,h2_block,sep='+'))
+as.formula(paste(base,h1_block,h2_block,sep='+'))
+
+period_break_ddates
+
+
+summary(test2)
+,R=10000)
+
+
+twopath +             
+    nodecov('High_Resource')+
     nodecov('Core_Order_Prior')+
     nodecov('High_Resource_x_Core_Order_Prior')+
-    memory(type = "autoregression", lag = 1)+
-    edgecov(stability_list_prior)+ 
-    timecov(x = stability_list_prior, minimum = 7, transform = function(t) 1) ,R = 1000)
+    nodeofactor('Utility') + nodeifactor('Utility') +
+    memory(type = "autoregression", lag = 1) +
+    edgecov(dynamic_list_prior)+ 
+    timecov(x = dynamic_list_prior,  minimum = 8, transform = function(t) 1),R=100)
+
+
+
+
+
+test2 = btergm(net_list[-1] ~ edges  + 
+                #Controls
+                mutual + gwidegree(2,fixed=T) +  gwodegree(2,fixed=T) + gwesp(1,fixed=T) + 
+  
+                #edgecov(utility_list_odegree) + 
+                #edgecov(utility_list_idegree) + 
+                #H5
+                nodeofactor('Signatory') + nodeifactor('Signatory') + 
+                #H3
+                edgecov(stability_list_prior) +  
+                #memory(type = "autoregression", lag = 1) +
+                #H1
+                edgecov(dynamic_list_prior)+ 
+                timecov(x = dynamic_list_prior,  minimum = 8, transform = function(t) 1) +
+                #H4
+                timecov(x = utility_list_odegree[-1],function(t) t),R=10000)
+
+
+?memory
+library(texreg)
+library(gwdegree)
+gwdegree()
+plotreg(test2)
+
+summary(test2)
+
+
+lapply(net_list,function(x) degreedist(x))
+
+
+
+    
+
+
+summary(test)
+data('knecht')
+demographics
+preprocess(node_base$Utility,net_list,lag=F,covariate = T)
+
+
+
+sex.cov <- preprocess(demographics$sex, primary.cov, friendship,
+                      + lag = FALSE, covariate = TRUE)
+    
+    
+    timecov(x = dynamic_list_prior,  transform = function(t) 1) ,R = 1000)
+
+
+
+
+
+degreedist(net_list[[3]])
+
+
 
 test@time.steps
 
@@ -126,8 +351,8 @@ sum(is.na(test@effects))
   #edgecov(yearly_outdegree_prior) + edgecov(yearly_attendance_prior)+
   #edgecov(yearly_outdegree_x_attendance_prior) +
   #  edgecov(yearly_outdegree_x_attendance_x_highres_prior) +
-  edgecov(stability_list_prior) + 
-  timecov(x = stability_list_prior, minimum = 4, transform = function(t) 1) ,R = 100)
+  edgecov(dynamic_list_prior) + 
+  timecov(x = dynamic_list_prior, minimum = 4, transform = function(t) 1) ,R = 100)
 
 summary(test)
 
@@ -145,7 +370,7 @@ summary(test)
 
 
 lapply(net_list,function(x) table(as.sociomatrix(x)>0))
-lapply(stability_list,function(x) table(x>0))
+lapply(dynamic_list,function(x) table(x>0))
 
 
 
@@ -162,14 +387,14 @@ lapply(test,sum)
 
 
 sum(as.sociomatrix(net_list[[1]]))
-sum(ifelse(stability_list[[2]]<0,0,stability_list[[2]]))
+sum(ifelse(dynamic_list[[2]]<0,0,dynamic_list[[2]]))
 
-stability_list[[1]][TRUE]<- (-1)
-stability_l
+dynamic_list[[1]][TRUE]<- (-1)
+dynamic_l
 
 
 library(btergm)
-stability_list[[1]]
+dynamic_list[[1]]
 set.seed(seed = 24)
 
 
@@ -184,7 +409,7 @@ test = lapply(net_list,as.sociomatrix)
 test = lapply(test,function(x) ifelse(x>0,1,-1))
 
 cbind(sapply(test,sum),
-sapply(stability_list,sum))
+sapply(dynamic_list,sum))
 
 data("knecht")
 lapply(friendship,dim)
@@ -192,13 +417,13 @@ lapply(friendship,dim)
 
 
 test = preprocess(lapply(net_list,as.sociomatrix),lag=TRUE,covariate=TRUE,
-           memory='stability')
+           memory='dynamic')
 
 class(test)
 
 
 mem <- preprocess(friendship, primary, demographics$sex,
-                   lag = TRUE, covariate = TRUE, memory = "stability",
+                   lag = TRUE, covariate = TRUE, memory = "dynamic",
                    na = NA, na.method = "fillmode", structzero = 10,
                    structzero.method = "remove")
 
@@ -217,19 +442,19 @@ and -1 if the previous dyad had a 0.
 
 
 See the attached paper for the mathematical details of the change statistic 
-for dyadic stability.
+for dyadic dynamic.
 
 You can include this list of matrices via an edgecov term.
 You can check if you did it right by comparing the results to a 
 model with the built-in memory term. 
 After that, include a timecov model term (see ?"btergm-terms" for details) with the following specification 
-to do the actual interaction (where 'ds' indicates the list of dyadic stability matrices):
+to do the actual interaction (where 'ds' indicates the list of dyadic dynamic matrices):
   
   timecov(x = ds, minimum = 6, transform = function(t) 1)
 
 This includes two model terms: the main effect for time, 
 where 0 is used for t=2 to t=5 and 1 is used for t=6 to t=10, and the 
-interaction between this term and the dyadic stability change statistic.
+interaction between this term and the dyadic dynamic change statistic.
 ?timecov
 
 
